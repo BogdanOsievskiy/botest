a90084e68eb0d06ec630ad91d6dfa281a1598611
OsBohdan <bohdan.osiievskyi@402.studio>
2026-01-18 15:10:43 +0200

    Update README small improvments

--- src/components/background-generator/generate-background-idea.tsx
+++ src/components/background-generator/generate-background-idea.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import actionButtonPrev from '@/assets/icons/action_back.svg'
 import actionButtonNext from '@/assets/icons/action_next.svg'
 import aiSVG from "@/assets/icons/AI.svg"
@@ -5,14 +7,35 @@ import buttonAIIcon from '@/assets/icons/AI_button.svg'
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 
+const testText = 'Animate glowing rays pulsating from behind the bottle, leaves gently swaying, and golden sparkles floating upward for a natural, radiant effect.';
+
+interface IconButtonProps {
+  img: string
+  onClick?: () => void
+}
+
+const IconButton = ({ img, onClick }: IconButtonProps) => {
+  return (
+    <Button
+      variant="ghost"
+      className="!bg-transparent !border-0 text-black w-[34px] h-[34px] !p-0 mx-1 pointer-events-auto"
+      onClick={onClick}
+    >
+      <img src={img} alt="" className="w-5 h-5" />
+    </Button>
+  )
+}
+
 export function GenerateBackgroundIdea() {
+  const [text, setText ] = useState('')
   return(
     <div>
       <div className="relative mb-6">
         <Textarea
           placeholder='Your background idea'
-          defaultValue={'Animate glowing rays pulsating from behind the bottle, leaves gently swaying, and golden sparkles floating upward for a natural, radiant effect.'}
+          value={text}
           className="h-[195px] rounded-xl pt-4 px-4 resize-none pb-20"
+          onChange={(e) => setText(e.target.value)}
           style={{
             scrollPaddingBottom: '50px',
           }}
@@ -25,12 +48,8 @@ export function GenerateBackgroundIdea() {
             </Button>
 
             <div className="flex pr-4 pointer-events-auto">
-              <Button variant="ghost" className="!bg-transparent !border-0 text-black w-[34px] h-[34px] !p-0 mx-1 pointer-events-auto">
-                <img src={actionButtonPrev} alt="actionButtonPrev" className="w-5 h-5" />
-              </Button>
-              <Button variant="ghost" className="!bg-transparent !border-0 text-black w-[34px] h-[34px] !p-0 mx-1 pointer-events-auto">
-                <img src={actionButtonNext} alt="actionButtonNext" className="w-5 h-5" />
-              </Button>
+              <IconButton img={actionButtonPrev} onClick={() => setText('')} />
+              <IconButton img={actionButtonNext} onClick={() => setText(testText)} />
             </div>
           </div>
         </div>
